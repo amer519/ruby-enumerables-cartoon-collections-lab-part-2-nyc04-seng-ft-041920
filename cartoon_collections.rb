a2b6f1d3a749b3c31db032953fa39e5473d712af
Amer Fahmy <afahmy519@gmail.com>
2020-05-02 02:53:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   planeteer_calls.find do |call|
-    call == "Earth!"
+    call == valid_calls
   end
 end
